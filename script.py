e7e4d014ec3f393c4dcf8660e67f71e1c6fc9794
Michelle Chen <chen.michelle15@gmail.com>
2020-08-21 03:14:52 -0400

    outputs to 3 diff files

--- script.py
+++ script.py
@@ -1,52 +1,70 @@
+import os
 import pandas
+import shutil
+from datetime import datetime
 from openpyxl import load_workbook
 
-def main():
+
+def main(dirname):
 	df = pandas.read_excel(io="raw_data_1.xlsx", sheet_name='Data')
 
 	# parsing input data
-	d1 = list(df['Geranic Acid|Amount'])[2:]
+	d1_label = df.iloc[1, 2].split(' ')[0]
+	d1 = list(df.iloc[2:, 5])
 	d1s1, d1s2, d1s3, d1b1, d1b2, d1b3 = save_data(d1)
 	d1b1_avg_list = calc_avg(d1b1)
 	d1b2_avg_list = calc_avg(d1b2)
 	d1b3_avg_list = calc_avg(d1b3)
 
-	d2 = list(df['Ibuprofen|Amount'])[2:]
+	d2_label = df.iloc[0, 2].split(' ')[0]
+	d2 = list(df.iloc[2:, 8])
 	d2s1, d2s2, d2s3, d2b1, d2b2, d2b3 = save_data(d2)
 	d2b1_avg_list = calc_avg(d2b1)
 	d2b2_avg_list = calc_avg(d2b2)
 	d2b3_avg_list = calc_avg(d2b3)
 
 	# from 48h sheet
-	ss = 5
 	df2 = pandas.read_excel(io="raw_data_2.xlsx", sheet_name='Data')
-	d1_48 = list(df2['Geranic Acid|Amount'])[2:]
-	save_48_data(d1_48, d1s1, d1s2, d1s3, d1b1, d1b2, d1b3,
-		d1b1_avg_list, d1b2_avg_list, d1b3_avg_list)
+	d1_48 = list(df2.iloc[2:, 5])
+	save_48_data(
+		d1_48, d1s1, d1s2, d1s3, d1b1, d1b2, d1b3,
+		d1b1_avg_list, d1b2_avg_list, d1b3_avg_list
+	)
 
-	d2_48 = list(df2['Ibuprofen|Amount'])[2:]
-	save_48_data(d2_48, d2s1, d2s2, d2s3, d2b1, d2b2, d2b3,
-		d2b1_avg_list, d2b2_avg_list, d2b3_avg_list)
+	d2_48 = list(df2.iloc[2:, 8])
+	save_48_data(
+		d2_48, d2s1, d2s2, d2s3, d2b1, d2b2, d2b3,
+		d2b1_avg_list, d2b2_avg_list, d2b3_avg_list
+	)
 
-	# outputting data
-
-	wb = load_workbook('empty_template.xlsx')
-
-	paste_blank_data(wb, 'Blank S1', d1b1)
-	paste_blank_data(wb, 'Blank S2', d2b1)
-	paste_data(wb, 'S1', d1s1, d1b1_avg_list)
-	paste_data(wb, 'S2', d2s1, d2b1_avg_list)
+	# write all data to 3 separate docs
+	output_data(
+		'%s/output1.xlsx' % dirname, d1_label, d2_label, d1s1, d2s1, d1b1, d2b1,
+		d1b1_avg_list, d2b1_avg_list
+	)
+	output_data(
+		'%s/output2.xlsx' % dirname, d1_label, d2_label, d1s2, d2s2, d1b2, d2b2,
+		d1b2_avg_list, d2b2_avg_list
+	)
+	output_data(
+		'%s/output3.xlsx' % dirname, d1_label, d2_label, d1s3, d2s3, d1b3, d2b3,
+		d1b3_avg_list, d2b3_avg_list
+	)
 
 
 def save_data(data):
 	ss = 15  # samplesize
-	return (data[0:ss], data[ss:ss * 2],
+	return (
+		data[0:ss], data[ss:ss * 2],
 		data[ss * 2:ss * 3], data[ss * 3:ss * 4],
-		data[ss * 4:ss * 5], data[ss * 5:ss * 6])
+		data[ss * 4:ss * 5], data[ss * 5:ss * 6]
+	)
 
 
-def save_48_data(data, s1, s2, s3, b1, b2, b3,
-		b1_avg_list, b2_avg_list, b3_avg_list):
+def save_48_data(
+	data, s1, s2, s3, b1, b2, b3,
+	b1_avg_list, b2_avg_list, b3_avg_list):
+
 	ss = 5
 	s1 += data[0:ss]
 	s2 += data[ss:ss * 2]
@@ -59,8 +77,32 @@ def save_48_data(data, s1, s2, s3, b1, b2, b3,
 	b3_avg_list += [sum(data[ss * 5: ss * 6]) / 5]
 
 
-def paste_blank_data(wb, worksheet, data):
-	ws = wb[worksheet]
+# returns list of averages
+def calc_avg(data):
+	lst = [0, 0, 0]
+	for i in range(0, 15, 3):
+		lst[0] += data[i]
+		lst[1] += data[i + 1]
+		lst[2] += data[i + 2]
+	return [x / 5 for x in lst]
+
+
+def output_data(
+	output_file, d1_label, d2_label, drug1, drug2, drug1_blank, drug2_blank,
+	drug1_blankavg, drug2_blankavg):
+	wb = load_workbook(output_file)
+	paste_blank_data(wb, d1_label, 'Blank S1', drug1_blank)
+	paste_blank_data(wb, d2_label, 'Blank S2', drug2_blank)
+	paste_data(wb, d1_label, 'S1', drug1, drug1_blankavg)
+	paste_data(wb, d2_label, 'S2', drug2, drug2_blankavg)
+	wb.save(output_file)
+
+
+def paste_blank_data(wb, label, worksheet, data):
+	try:
+		ws = wb[worksheet]
+	except:
+		ws = wb[label + ' Blank']
 	i = 0
 	for row in ws.iter_rows(min_row=5, min_col=8, max_row=9, max_col=10):
 		for cell in row:
@@ -70,15 +112,17 @@ def paste_blank_data(wb, worksheet, data):
 		for cell in col:
 			cell.value = data[i]
 			i += 1
-	wb.save('empty_template.xlsx')
+	ws.title = label + ' Blank'
 
 
-def paste_data(wb, worksheet, data, avg_list):
-	ws = wb[worksheet]
+def paste_data(wb, label, worksheet, data, avg_list):
+	try:
+		ws = wb[worksheet]
+	except:
+		ws = wb[label + ' Sample']
 	i, j = 0, 0
 	for row in ws.iter_rows(min_row=5, min_col=8, max_row=9, max_col=10):
 		for cell in row:
-			# print('for ' , cell , data[i] , avg_list[j])
 			if data[i] - avg_list[j] < 0:
 				cell.value = data[i]
 			else:
@@ -92,17 +136,20 @@ def paste_data(wb, worksheet, data, avg_list):
 			else:
 				cell.value = data[i] - avg_list[3]
 			i += 1
-	wb.save('empty_template.xlsx')
-
-# returns list of averages
-def calc_avg(data):
-	lst = [0, 0, 0]
-	for i in range(0, 15, 3):
-		lst[0] += data[i]
-		lst[1] += data[i + 1]
-		lst[2] += data[i + 2]
-	return [x / 5 for x in lst]
+	ws.title = label + ' Sample'
 
 
 if __name__ == "__main__":
-	main()
+	# raw_data1 = input('Enter the name of the 2-24h raw data file: ')
+	# raw_data2 = input('Enter the name of the 48h raw data file: ')
+	try:
+		dirname = datetime.today().strftime('%Y%m%d') + ' Analysis'
+		os.mkdir(dirname)
+		shutil.copy2('empty_template.xlsx', '%s/output1.xlsx' % dirname)
+		shutil.copy2('empty_template.xlsx', '%s/output2.xlsx' % dirname)
+		shutil.copy2('empty_template.xlsx', '%s/output3.xlsx' % dirname)
+	except:
+		print('Files exist. Appending files instead')
+		pass
+
+	main(dirname)
